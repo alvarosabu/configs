4150ee547e7e5cb15e22d570cf2ccd4526d21220
Alvaro <alvaro.saburido@gmail.com>
2021-07-14 10:52:15 +0200

    feat: vue config

---
+++ packages/vue/README.md
@@ -0,0 +1,11 @@
+# `eslint-base`
+
+> TODO: description
+
+## Usage
+
+```
+const eslintBase = require('eslint-base');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/vue/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: ['@asigloo/eslint-base', 'plugin:vue/vue3-recommended'],
+  plugins: ['vue'],
+}
